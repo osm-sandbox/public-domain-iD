803d9d7a0ef56ccbf22ba394ed90130b2906e081
Quincy Morgan <2046746+quincylvania@users.noreply.github.com>
2022-08-25 15:10:04 -0300

    Replace some OSM links with PD map links

--- pd_locales.js
+++ pd_locales.js
@@ -8,7 +8,13 @@ fs.readdir(dir, (err, files) => {
     const path = dir + '/' + file;
     fs.readFile(path, (err, data) => {
       if (err) throw err;
-      fs.writeFile(path, new String(data).replace(/OpenStreetMap/g, 'PublicDomainMap'), (err) => {
+      var out = new String(data)
+        .replace(/OpenStreetMap/g, 'PublicDomainMap')
+        .replace(/\(https:\/\/www.openstreetmap.org\/\)/g, '(https://www.publicdomainmap.org/)')
+        .replace(/https:\/\/www.openstreetmap.org\/copyright/g, 'https://www.publicdomainmap.org/license')
+        .replace(/\(https:\/\/github.com\/openstreetmap\/iD\)/g, '(https://github.com/publicdomainmap/editor)');
+
+      fs.writeFile(path, out, (err) => {
         if (err) throw err;
       });
     });
