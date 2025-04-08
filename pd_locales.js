c48259608efe1056d7490a6ca2d9f180b766f279
Quincy Morgan <2046746+quincylvania@users.noreply.github.com>
2025-04-08 15:28:33 -0400

    Update endpoints

--- pd_locales.js
+++ pd_locales.js
@@ -9,13 +9,13 @@ fs.readdir(dir, (err, files) => {
     fs.readFile(path, (err, data) => {
       if (err) throw err;
       var out = new String(data)
-        .replace(/\(https:\/\/github.com\/openstreetmap\/iD\)/g, '(https://github.com/osm-sandbox/public-domain-id)')
-        .replace(/OpenStreetMap/g, 'Public Domain Map')
+        .replace(/\(https:\/\/github.com\/openstreetmap\/iD\)/g, '(https://github.com/osm-sandbox/sandbox-iD)')
+        .replace(/OpenStreetMap/g, 'OSM Sandbox')
         .replace(/https:\/\/www.openstreetmap.org\/copyright/g, 'https://www.publicdomainmap.org/license')
         .replace(/\(https:\/\/www.openstreetmap.org\/\)/g, '(https://www.publicdomainmap.org/)')
         // FIXME - these two aren't multi-language
-        .replace(/on openstreetmap.org/g, 'on publicdomainmap.org')
-        .replace(/History on osm.org/g, 'History on publicdomainmap.org');
+        .replace(/on openstreetmap.org/g, 'on osmsandbox.us')
+        .replace(/History on osm.org/g, 'History on osmsandbox.us');
 
       fs.writeFile(path, out, (err) => {
         if (err) throw err;
